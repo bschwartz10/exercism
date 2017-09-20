ad899bebd5ec31a175fcc0f731a1ec7dcb6ce813
bschwartz10 <bschwartz92@gmail.com>
2017-09-20 11:25:19 -0400

    JS Pangram

---
+++ ecmascript/pangram/pangram.js
@@ -0,0 +1,16 @@
+module.exports = class Pangram {
+  constructor (sentence) {
+    this.sentence = sentence.toLowerCase()
+    this.alphabet = [
+                      'a','b','c','d','e','f','g','h','i',
+                      'j','k','l','m','n','o','p','q','r',
+                      's','t','u','v','w','x','y','z'
+                    ]
+  }
+
+  isPangram() {
+    return this.alphabet.every((letter) => {
+      return this.sentence.includes(letter)
+    })
+  }
+}
