9f55893db3d7d10f3e766bf54ee14f86ca5da19d
bschwartz10 <bschwartz92@gmail.com>
2018-09-12 11:01:33 -0400

    hamming refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,26 +1,13 @@
-require_relative 'book_keeping'
-
 class Hamming
-  include BookKeeping
-
-  def self.compute(first_strand, second_strand)
-    check_length(first_strand, second_strand)
-
-    counter = 0
-    index = 0
-    length = first_strand.length
-
-    length.times do
-      counter += 1 if first_strand[index] != second_strand[index]
-      index += 1
-    end
-    counter
+  def self.compute(strand_one, strand_two)
+    raise ArgumentError if strand_one.length != strand_two.length
+    length = strand_one.length
+    difference = 0
+    length.times {|i| difference += 1 if strand_one[i] != strand_two[i]}
+    difference
   end
+end
 
-  def self.check_length(first_strand, second_strand)
-    if first_strand.length != second_strand.length
-      raise ArgumentError.new
-    end
-  end
-
+module BookKeeping
+  VERSION = 3
 end
